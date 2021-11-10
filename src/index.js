8533a35a9250b32d289b0acf7f4f45ff808812ab
davinci360 <jasonbrowntime@gmail.com>
2021-11-10 11:53:17 -0500

    stop time

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 /*Core Deliverables
@@ -14,3 +15,14 @@ I can mark a plant as "sold out".
 I can search for plants by their name and see a filtered list of plants.*/
 
 /*When the app starts user sees all plants, fetch request required*/
+{
+    useEffect(() => {
+        fetch("http://localhost:6001/plants")
+            .then((response) => response.json())
+            .then((plants) => {
+        setPlants(data.plants);
+            });	
+    }, []);
+
+}
+// Its coming back to me now... a few practice runs/ review notes and lectures should be A OK for Friday..! 
