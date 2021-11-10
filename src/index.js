769d3a04cb3382e10fc5596f2dc9b78a63b2ad3c
davinci360 <jasonbrowntime@gmail.com>
2021-11-10 11:18:27 -0500

    App Compiled Warning cleared

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import "./index.css";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+/*Core Deliverables
+As a user:
+
+When the app starts, I can see all plants.
+I can add a new plant to the page by submitting the form.
+I can mark a plant as "sold out".
+I can search for plants by their name and see a filtered list of plants.*/
+
+/*When the app starts user sees all plants, fetch request required*/
